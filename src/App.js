b6792fc1633ca934cff5b072c526c9772279d9eb
Bryan Mayson <maysonbryan@gmail.com>
2021-01-15 07:53:15 +0700

    form styling change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
-import {BrowserRouter as Switch,Route,useLocation} from 'react-router-dom';
+import {BrowserRouter as Switch,Redirect,Route,useLocation} from 'react-router-dom';
 import RegisterForm from './components/RegisterForm';
 import { AnimatePresence } from 'framer-motion';
+import {useState} from 'react';
 
 
 function App() {
   const location =useLocation();
+  const [loggedIn , updateLogin] = useState(false);
 
   return (
     <div className="App">
           <AnimatePresence exitBeforeEnter >
             <Switch location={location} key={location.key}>  
-              <Route exact path="/" component={LoginForm}/>
-              <Route path="/register" component={RegisterForm}/>
-
+              <Route exact path="/">
+                  {loggedIn?<Redirect to="/homepage"/> : <LoginForm/> }</Route>
+              <Route/>
+              <Route exact path="/register">
+                  {loggedIn?<Redirect to="/homepage"/> : <RegisterForm/> }</Route>
+              <Route/>
             </Switch>
           </AnimatePresence>
-      <div className="login-image"></div>
+
     </div>
   );
 }
